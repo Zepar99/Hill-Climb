9db3e51559373d64b05e6ccaa6286a3620ab6d11
Zepar99 <pratikkborse@gmail.com>
2022-03-31 21:56:19 +0530

    UI Design

---
+++ Assets/_Project/Scripts/GameOver.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public static bool GameIsOver = false;
+    [SerializeField] private GameObject GameoverUI;
+    private CarController car;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            gameOver();
+        }
+    }
+    private void gameOverOnFuel()
+    {
+        if (car.fuel < 0)
+        {
+            gameOver();
+        }
+    }
+    private void gameOver()
+    {
+        GameIsOver = true;
+        GameoverUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    private void Restart()
+    {
+        GameIsOver = false;
+        GameoverUI.SetActive(false);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    private void Quit()
+    {
+        Application.Quit();
+    }
+}
